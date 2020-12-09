6c8b0b4e2d18d46718b31ab362d1103ec62a3358
Pablo Sanchez Alonso <pablo.sanchez@codurance.com>
2020-12-09 15:43:06 +0000

    Using in memory database for tests

--- acceptance/src/test/java/com/codurance/hotel/CorporateHotelApplicationTests.java
+++ acceptance/src/test/java/com/codurance/hotel/CorporateHotelApplicationTests.java
@@ -2,8 +2,10 @@ package com.codurance.hotel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CorporateHotelApplicationTests {
 
   @Test
